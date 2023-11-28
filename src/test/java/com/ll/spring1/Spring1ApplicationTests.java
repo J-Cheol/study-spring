96fc241f5066af813d9ec02a118808f74242e87d
J-Cheol <lunaris0206@naver.com>
2023-11-28 15:22:14 +0900

    start

---
+++ src/test/java/com/ll/spring1/Spring1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.spring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
