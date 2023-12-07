ad0aecf6543f9f4221cb69c5aab2f5560d792c3f
J-Cheol <lunaris0206@naver.com>
2023-12-07 14:31:09 +0900

    DI

--- src/main/java/com/ll/spring1/service/MemberService.java
+++ src/main/java/com/ll/spring1/service/MemberService.java
@@ -2,19 +2,23 @@ package com.ll.spring1.service;
 
 import com.ll.spring1.domain.Member;
 import com.ll.spring1.repository.MemberRepository;
-import com.ll.spring1.repository.MemoryMemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
 public class MemberService
 {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository)
+    {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
